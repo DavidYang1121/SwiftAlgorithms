620a91a642152938b96e64ef7638ba0eab6128f8
David Yang <yangwei@ymatou.com>
2019-08-07 19:00:08 +0800

    FindAllDuplicates

---
+++ Algorithms/Algorithms/Array/FindAllDuplicates.swift
@@ -0,0 +1,29 @@
+//
+//  FindAllDuplicates.swift
+//  Algorithms
+//
+//  Created by DavidYang on 2019/8/7.
+//  Copyright © 2019 cocoapods. All rights reserved.
+//
+
+import Foundation
+
+class FindAllDuplicates {
+    func findDuplicates(_ nums: [Int]) -> [Int] {
+        guard nums.count > 1 else {
+            return []
+        }
+        var result: [Int] = []
+        var nums = nums.sorted()
+        var times = 1
+        for i in 1..<nums.count {
+            if (nums[i] != nums[i - 1]) {
+                times = 1
+            } else {
+                times += 1
+                result.append(nums[i])
+            }
+        }
+        return result
+    }
+}
