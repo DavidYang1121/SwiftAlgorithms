82fc4310884ceae949a89306c778ce2a103af03f
David Yang <yangwei@ymatou.com>
2019-11-28 17:26:49 +0800

    update counting sort

--- Algorithms/Algorithms/Sort/CountingSort.swift
+++ Algorithms/Algorithms/Sort/CountingSort.swift
@@ -10,32 +10,33 @@ import Foundation
 
 
 func countingSort(_ a: inout [Int]) {
-    guard a.count > 1 else {
-        return
-    }
-    
-    //找出范围[0-max]
-    var max = a[0]
-    for i in 1..<a.count {
-        if a[i] > max {
-            max = a[i]
-        }
-    }
-    
-    //生成(max + 1)个计数桶
-    var counting = [Int](repeating: 0, count: max + 1)
-    for i in 0..<a.count {
-        counting[a[i]] += 1
-    }
-    
-    //还原排序数组
-    var result: [Int] = []
-    for i in 0..<counting.count {
-        var value = counting[i]
-        while value > 0 {
-            result.append(i)
-            value -= 1
-        }
-    }
-    a = result
+     guard a.count > 1 else {
+          return
+      }
+      
+      //找出范围[0-max]
+      var max = a[0]
+      for i in 1..<a.count {
+          if a[i] > max {
+              max = a[i]
+          }
+      }
+      
+      //生成(max + 1)个计数桶
+      var counting = [Int](repeating: 0, count: max + 1)
+      for i in 0..<a.count {
+          counting[a[i]] += 1
+      }
+      
+      for i in 1..<counting.count {
+          counting[i] += counting[i - 1]
+      }
+      
+      //还原排序数组
+      var result = [Int](repeating: 0, count: a.count)
+      for e in a {
+          counting[e] -= 1
+          result[counting[e]] = e
+      }
+      a = result
 }
